15206e240894bb34f2ee2e9f5192442bcc9680c6
JMustang <eecfredes@gmail.com>
2022-03-30 22:40:34 -0300

    First Day

---
+++ FirstDay.py
@@ -0,0 +1,30 @@
+# # print function
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# # print the length of the string
+# name = input("What is your name? ")
+# print("Your name has "+str(len(name))+" characters")
+
+# # switch values using in variable
+# a = input("a: ")
+# b = input("b: ")
+
+# a,b = b,a
+
+# print(a)
+# print(b)
+
+
+
+
+# time_until_midnight = "5"
+# print("There are " + time_until_midnight + " hours until midnight")
+
+print("welcome to the band name gernerator")
+
+city = input("Which city did you grow up in?\n ")
+
+pet = input("What is your pet's name?\n ")
+print("Your band name is " + city + pet)
