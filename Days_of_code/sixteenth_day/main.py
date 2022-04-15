2b95e82a4a8049a4921f1363eae2f65efda3f4d9
JMustang <eecfredes@gmail.com>
2022-04-14 21:06:09 -0300

    Sixteenth day

---
+++ Days_of_code/sixteenth_day/main.py
@@ -0,0 +1,14 @@
+# Studing Object Oriented Programming in Python
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable(['City name', 'Area', 'Population', 'Annual Rainfall'])
