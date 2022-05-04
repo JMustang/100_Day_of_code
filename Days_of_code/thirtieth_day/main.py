737b573275d8624ba199a1075553b608fee0e328
JMustang <eecfredes@gmail.com>
2022-05-04 19:56:58 -0300

    update

--- Days_of_code/thirtieth_day/main.py
+++ Days_of_code/thirtieth_day/main.py
@@ -1,15 +1,28 @@
-try:
-    file = open('data.txt')
-    data_dict = {'key': 'value'}
-    print(data_dict['key'])
-except FileNotFoundError:
-    file = open('data.txt', 'w')
-    file.write('Something')
-except KeyError as error_message:
-    print(f'The key {error_message} does not exist.')
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print('File was closed')
+#!/bin/python
+
+
+# try:
+#     file = open('data.txt')
+#     data_dict = {'key': 'value'}
+#     print(data_dict['key'])
+# except FileNotFoundError:
+#     file = open('data.txt', 'w')
+#     file.write('Something')
+# except KeyError as error_message:
+#     print(f'The key {error_message} does not exist.')
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print('File was closed')
+
+
+height = float(input('Enter your height: '))
+weight = int(input('Enter your weight: '))
+
+if height > 3:
+    raise ValueError('Height must be less than 3')
+
+bmi = weight / (height ** 2)
+print(f'Your BMI is {bmi}')
